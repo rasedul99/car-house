9edaad3fa29956e6d1c7e2eb81970163b490f67a
rasedul99 <rasedul2199@gmail.com>
2022-05-10 23:48:38 +0600

    signup signin page ui  added

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import "./SignIn.css";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div className="form-container">
+      <form>
+        <h3 className="text-center">Sign In</h3>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+        <div className="mb-3"></div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Forgot <a href="#">password?</a>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
